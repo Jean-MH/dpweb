f4f336007aabcce9bca57791de403948722b7edf
Jean-MH <jahuaman83@gmail.com>
2025-05-30 12:18:55 -0500

    hl

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+const BD_HOST = 'localhost';
+const BD_NAME = 'inventario';
+const BD_USER = 'root';
+const BD_PASSWORD = 'root';
+const BD_CHARSET = 'utf8';
+
+//Ingresar link de proyecto
+const BASE_URL = 'http://localhost/dpweb/';
+?>
