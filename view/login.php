1c20437311012c49b19eff91fc88f507e56efdd5
Jean-MH <jahuaman83@gmail.com>
2025-11-04 10:50:42 -0500

    Update login.php

--- view/login.php
+++ view/login.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     body {
-      background:gradient(to right, #8eb1f3ff, #000000ff);
+      background:blue;
       min-height: 100vh;
       display: flex;
       align-items: center;
